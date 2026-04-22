37aa2a1b548b8513012d607ce99056ac9699ab47
HectorE23 <hrodz023@gmail.com>
2026-04-22 14:39:30 -0400

    Create 58_Length_of_Last_Word.py
    add length of last word

---
+++ Easy/58_Length_of_Last_Word.py
@@ -0,0 +1,6 @@
+# 58. Length of Last Word
+# https://leetcode.com/problems/length-of-last-word/
+
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        return len(s.strip().split()[-1])
