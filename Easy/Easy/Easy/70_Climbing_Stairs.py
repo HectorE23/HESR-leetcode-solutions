f481e356680d71bfac461d95265ddc010b5b5ab6
HectorE23 <hrodz023@gmail.com>
2026-04-22 14:40:38 -0400

    Create 70_Climbing_Stairs.py
    add climbing stairs

---
+++ Easy/Easy/Easy/70_Climbing_Stairs.py
@@ -0,0 +1,14 @@
+# 70. Climbing Stairs
+# https://leetcode.com/problems/climbing-stairs/
+
+class Solution(object):
+    def climbStairs(self, n):
+        if n <= 2:
+            return n
+
+        a, b = 1, 2
+
+        for _ in range(3, n + 1):
+            a, b = b, a + b
+
+        return b
